a2b68f6dcb5c643220810d54eb85a8d66950631b
sethdanny <naddulidaniel1994@gmail.com>
2023-10-19 01:41:52 +0300

    start on routes and error handling

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandler = require('./middleware/error-handler');
+
 const port = process.env.PORT || 5001;
 
+//routes
 app.get('/', (req, res) => {
-  res.send('Welcome to the store');
+  res.send('<h1>Store API </h1> <ahref="/api/v1/products"> Products route</a>');
 });
 
 // middleware
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.use(notFoundMiddleware);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
